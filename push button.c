629412d2792ebefa6cd28c259c36d34806dab727
dani0303 <48264113+dani0303@users.noreply.github.com>
2019-12-08 16:24:38 -0500

    Update push button.c

--- push button.c
+++ push button.c
@@ -1,5 +1,28 @@
 #include <kipr/botball,h>
  
- int button = 3;
+int button = 3;
+int r_motor = 0;
+int l_motor = 1;
+
+int main()
  
+{
+  printf("push the button to execute the robot");
+ 
+ if(analog(3) == 1)
+ {
+     mav(r_motor, 12000);
+     mav(l_motor, 12000);
+     msleep(5000);
+     mav(r_motor, -12000);
+     mav(l_motor, 12000);
+     msleep(1500);
+ }
+
+ else-if(analog(3) == 0)
+ {
+     printf("push the button!");
+ }
+    return 0;
+}
  
